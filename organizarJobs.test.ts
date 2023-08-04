4a405488820f2155033bedd7b3cd9846057090d5
LuisSempre <luis.sempre@viannasempre.com.br>
2023-08-03 22:26:29 -0300

    fix: Removendo comentarios

--- organizarJobs.test.ts
+++ organizarJobs.test.ts
@@ -42,20 +42,16 @@ test('Teste com jobs que precisam ser divididos em mais de um array', () => {
 
    const resultado = organizarJobs(jobs);
 
-   // Test the length of the resulting array
    expect(resultado.length).toBe(2);
  
-   // Test the total number of jobs in the resulting arrays
    const totalJobsInResultado = resultado.reduce((acc, array) => acc + array.length, 0);
    expect(totalJobsInResultado).toBe(jobs.length);
- 
-   // Test the total time of jobs in each array
+
    const totalTimeArray1 = resultado[0].reduce((acc, job) => acc + new Date(job["Tempo estimado"]).getTime(), 0);
    const totalTimeArray2 = resultado[1].reduce((acc, job) => acc + new Date(job["Tempo estimado"]).getTime(), 0);
    expect(totalTimeArray1).toBeLessThanOrEqual(8 * 60 * 60 * 1000);
    expect(totalTimeArray2).toBeLessThanOrEqual(8 * 60 * 60 * 1000);
  
-   // Test the order of jobs in each array based on "Data Máxima de conclusão"
    const dataArray1 = resultado[0].map(job => new Date(job["Data Máxima de conclusão"]).getTime());
    const dataArray2 = resultado[1].map(job => new Date(job["Data Máxima de conclusão"]).getTime());
    expect(dataArray1).toEqual(dataArray1.slice().sort((a, b) => a - b));
